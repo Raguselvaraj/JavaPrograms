54fb5930ec5905de72d9c3318cb4d74fe02e1477
Ragu <mailmeragu92@gmail.com>
2021-01-18 14:47:22 +0530

    Done

---
+++ src/com/javas/OddOrEven.java
@@ -0,0 +1,24 @@
+package com.javas;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the name");
+		String next = sc.nextLine();
+		System.out.println(next);
+		System.out.println("Enter the id");
+		byte next1 = sc.nextByte();
+		System.out.println(next1);
+		System.out.println("Enter the num");
+		int next2 = sc.nextInt();
+		System.out.println(next2);
+		
+	
+	
+	}
+
+}
